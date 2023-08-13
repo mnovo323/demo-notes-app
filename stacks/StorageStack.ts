417bd53e797088c285b618c54d30b32c5cd99870
Michael Novotny <323mtn@gmail.com>
2023-08-13 12:26:00 -0400

    Enabling CORS

--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -4,7 +4,16 @@ export function StorageStack({ stack, app }: StackContext): {
   table: Table;
   bucket: Bucket;
 } {
-  const bucket = new Bucket(stack, 'Uploads');
+  const bucket = new Bucket(stack, 'Uploads', {
+    cors: [
+      {
+        maxAge: '1 day',
+        allowedOrigins: ['*'],
+        allowedHeaders: ['*'],
+        allowedMethods: ['GET', 'PUT', 'POST', 'DELETE', 'HEAD'],
+      },
+    ],
+  });
   const table = new Table(stack, 'Notes', {
     fields: {
       userId: 'string',
