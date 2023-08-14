a2ca800ea27ac142b5c162e1965b6d05bd5a0b9f
Michael Novotny <323mtn@gmail.com>
2023-08-14 18:41:38 -0400

    more building yo

--- packages/functions/src/update.ts
+++ packages/functions/src/update.ts
@@ -4,11 +4,16 @@ import dynamoDb from '@notes/core/dynamodb';
 
 export const main = handler(async (event) => {
   const data = JSON.parse(event.body);
+  const userId =
+    event.requestContext.authorizer?.iam.cognitoIdentity.identityId;
+  if (!userId) {
+    throw new Error('Missing user id');
+  }
   const params = {
     TableName: Table.Notes.tableName,
     // 'Key' defines the partition key and sort key of the item to be updated
     Key: {
-      userId: '123', // The id of the author
+      userId: userId, // The id of the author
       noteId: event.pathParameters.id, // The id of the note from the path
     },
     // 'UpdateExpression' defines the attributes to be updated
